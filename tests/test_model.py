c7c5f3e70959021fd6ece2e61791cbc866c377d8
Francesco Terenzi <fra.terenz1993@gmail.com>
2025-08-17 16:25:01 +0100

    ruff checks

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,14 @@
-import pytest
 import torch
 
 from adversarial.model import ResNet50
 
+
 def test_resnet50_init():
     my_model = ResNet50()
-    torch.testing.assert_close(my_model.mean, torch.tensor([0.485, 0.456, 0.406]).view_as(my_model.mean))
-    torch.testing.assert_close(my_model.std, torch.tensor([0.229, 0.224, 0.225]).view_as(my_model.std))
+    torch.testing.assert_close(
+        my_model.mean, torch.tensor([0.485, 0.456, 0.406]).view_as(my_model.mean)
+    )
+    torch.testing.assert_close(
+        my_model.std, torch.tensor([0.229, 0.224, 0.225]).view_as(my_model.std)
+    )
     assert len(my_model.meta["categories"]) == 1000
-
